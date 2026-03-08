8e6307d30b4b97904e32e5e0488734e33242b1c6
Autumn <auctumnus@pm.me>
2026-03-08 16:31:44 -0500

    tables

--- src/table.rs
+++ src/table.rs
@@ -3,50 +3,76 @@
 use std::collections::{BTreeSet, HashMap, HashSet};
 
 use crate::{
-    Phoneme, feature::{
-        Airstream, ConsonantFeature, Depth, Feature, FricativeKind, Height, Manner, Modifier, Place, RearArticulation, Voice
-    }
+    feature::{
+        Airstream, ConsonantFeature, Depth, Feature, FricativeKind, Height, Manner, Modifier,
+        Place, PlaceCategory, RearArticulation, Voice,
+    },
+    Phoneme,
 };
+
+pub enum Heading {
+    Feature(Feature),
+    Category(PlaceCategory),
+}
+
+impl Heading {
+    fn try_merge_with(&self, other: &Heading) -> Option<(Heading, f32)> {
+        use ConsonantFeature::Place;
+        use Feature::Consonant;
+        match (self, other) {
+            (Heading::Feature(Consonant(Place(p1))), Heading::Feature(Consonant(Place(p2)))) => {
+                if p1.category() == p2.category() {
+                    Some((Heading::Category(p1.category()), 0.0))
+                } else {
+                    None
+                }
+            }
+            (Heading::Feature(Consonant(Place(place))), Heading::Category(cat))
+            | (Heading::Category(cat), Heading::Feature(Consonant(Place(place)))) => {
+                if place.category() == *cat {
+                    Some((Heading::Category(*cat), 0.0))
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+}
 
 pub struct Cell {
     pub phonemes: Vec<Phoneme>,
 }
 
 pub enum Row {
-    Group {
-        feature: Feature,
-        rows: Vec<Row>,
-    },
-    Individual {
-        feature: Feature,
-        cells: Vec<Cell>,
-    }
+    Group { heading: Heading, rows: Vec<Row> },
+    Individual { heading: Heading, cells: Vec<Cell> },
 }
 
 impl Row {
-    fn feature(&self) -> &Feature {
+    fn heading(&self) -> &Heading {
         match self {
-            Row::Group { feature, .. } => feature,
-            Row::Individual { feature, .. } => feature,
+            Row::Group { heading, .. } => heading,
+            Row::Individual { heading, .. } => heading,
         }
     }
 }
 
 pub enum Column {
     Group {
-        feature: Feature,
+        heading: Heading,
         columns: Vec<Column>,
     },
     Individual {
-        feature: Feature,
-    }
+        heading: Heading,
+    },
 }
 
 impl Column {
-    fn feature(&self) -> &Feature {
+    fn heading(&self) -> &Heading {
         match self {
-            Column::Group { feature, .. } => feature,
-            Column::Individual { feature } => feature,
+            Column::Group { heading, .. } => heading,
+            Column::Individual { heading } => heading,
         }
     }
 }
@@ -61,12 +87,14 @@ impl Table {
         let (phonemes, manners, places) = phonemes
             .iter()
             .filter_map(|p| {
-                let (manner, place) = p.features().iter().fold((None, None), |(m, p), f| {
-                    match f {
-                        Feature::Consonant(ConsonantFeature::Manner()) => (Some(manner), p),
-                        Feature::Place(place) => (m, Some(place)),
-                        _ => (m, p),
+                use ConsonantFeature::{DoubleArticulation, Manner, Place};
+                use Feature::Consonant;
+                let (manner, place) = p.features().iter().fold((None, None), |(m, p), f| match f {
+                    manner @ Consonant(Manner(..)) => (Some(manner), p),
+                    place @ (Consonant(Place(..)) | Consonant(DoubleArticulation(..))) => {
+                        (m, Some(place))
                     }
+                    _ => (m, p),
                 });
 
                 if let (Some(manner), Some(place)) = (manner, place) {
@@ -75,14 +103,16 @@ impl Table {
                     None
                 }
             })
-            .fold((Vec::new(), BTreeSet::new(), BTreeSet::new()), |(mut ps, mut ms, mut ps_), (p, m, p_)| {
-                ps.push(p.clone());
-                ms.insert(m);
-                ps_.insert(p_);
-                (ps, ms, ps_)
-            });
+            .fold(
+                (Vec::new(), BTreeSet::new(), BTreeSet::new()),
+                |(mut ps, mut ms, mut ps_), (p, m, p_)| {
+                    ps.push(p.clone());
+                    ms.insert(m);
+                    ps_.insert(p_);
+                    (ps, ms, ps_)
+                },
+            );
 
         todo!()
     }
 }
-
